cabeac92d4abd2c1f1621bdd37fec80809a0f523
WWWolkodaWWW <lilit_volkodav@mail.ru>
2022-09-02 16:51:11 +0200

    feat: add slider

--- travel/js/slider.js
+++ travel/js/slider.js
@@ -3,16 +3,35 @@ const slider = document.querySelector('.slider'),
 	nextBtn = document.querySelector('.arrow_right'),
 	pagination = document.querySelectorAll('.btn_pagination'),
 	card = document.querySelectorAll('.card');
+	// sliderImg = document.querySelectorAll();
 
-let slide = 0;
+let slide = 1;
+const size = card[0].clientWidth;
 
-function slideLeft() { 
-	
-}
+
+
+// slider.style.transform = 'translateX(' + (-size * slide) + 'px)';
 
 function slideRight() { 
-	card.style.left = `360px`; 
+
+	for (i = 0; i < card.length; i++) { 
+		card[i].style.transform = 'translateX(' + (-size * slide) + 'px)';
+	}
+
+
+
+
+	card.forEach(cart => { 
+		// cart.style.transform = 'translateX(' + (-size * slide) + 'px)';
+		
+
+
+	})
 }
 
-prevBtn.addEventListener('click', slideLeft);
+// function slideRight() { 
+// 	card.style.left = `370px`; 
+// }
+
+// prevBtn.addEventListener('click', slideLeft);
 nextBtn.addEventListener('click', slideRight);
