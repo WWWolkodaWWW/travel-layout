983575c9ec0a63803e15a4672123951811b87dbc
WWWolkodaWWW <lilit_volkodav@mail.ru>
2022-09-12 14:48:21 +0200

    feat: add popup modal windows (desktop version)

---
+++ travel/js/popup.js
@@ -0,0 +1,8 @@
+const popUp = document.querySelector('.popup'),
+	signUpForm = document.querySelector('.popup__signUp'),
+	logInForm = document.querySelector('.popup__logIn'),
+	btnLogIn = document.querySelector('.btn_login'),
+	signUpLink = document.querySelector('.signUp_link'),
+	btnSignIn = document.querySelector('.btn_signIn');
+
+
